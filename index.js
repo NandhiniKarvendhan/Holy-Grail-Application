71683a271ecdb1c7fcc0fa6b4fc184edfa26266b
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-01-28 14:19:52 +0530

    Adding index.js file to create server

--- index.js
+++ index.js
@@ -1,64 +1,10 @@
-function PlusMinus(props) {
-  function handle(e) {
-    if (e.target.id.includes("minus")) {
-      props.handle({ name: props.section, value: -1 });
-      return;
-    }
-    props.handle({ name: props.section, value: 1 });
-  }
-  return (
-    <>
-      <img
-        src={`icons/${props.section}_plus.png`}
-        id="plus"
-        onClick={(e) => handle(e)}
-      />
-      <img
-        src={`icons/${props.section}_minus.png`}
-        id="minus"
-        onClick={(e) => handle(e)}
-      />
-    </>
-  );
-}
+var express = require("express");
+var app = express();
 
-function Data(props) {
-  return (
-    <div>
-      Header: {props.data.header}, Left: {props.data.left}, Article:
-      {props.data.article}, Right: {props.data.right}, Footer:
-      {props.data.footer}
-    </div>
-  );
-}
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-function App() {
-  const [data, setData] = React.useState({
-    header: 0,
-    left: 0,
-    article: 0,
-    right: 0,
-    footer: 0,
-  });
-
-  function handle(section) {
-    console.log("pong", section);
-    const value = data[section.name] + section.value;
-    const object = { [section.name]: value };
-    setData({ ...data, ...object });
-  }
-
-  return (
-    <>
-      <div className="grid">
-        <Header handle={handle} data={data} />
-        <Left handle={handle} data={data} />
-        <Article handle={handle} data={data} />
-        <Right handle={handle} data={data} />
-        <Footer handle={handle} data={data} />
-      </div>
-    </>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
