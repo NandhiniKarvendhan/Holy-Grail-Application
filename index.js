5762407cf9d478428728f8ea70890d3534c0164f
NandhiniKarvendhan <nandhukannan10@gmail.com>
2023-01-29 21:12:28 +0530

    Updating the file

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require("express");
 var app = express();
+var redis = require("redis");
+var client = redis.createClient();
+
+// serve static files from public directory
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
